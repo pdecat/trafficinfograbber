608daebaa3262adca4813e4947989d69874cc086
pdecat <pdecat@48d36ac8-552b-11de-ac5b-d5db2185a7fc>
2009-10-19 18:42:00 +0000

    Renommage en Sytadroid Correction du positionnement par défaut Affichage allégé plus rapide avec cache des resources Passage en API 1.6

---
+++ sandbox/HelloAndroid/src/org/decat/sytadroid/Sytadroid.java
@@ -0,0 +1,254 @@
+package org.decat.sytadroid;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.WebHistoryItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Sytadroid extends Activity {
+	private static final String SYTADROID = "Sytadroid";
+	private static final String FILENAME_FOND_IDF = "fond_IDF.jpg";
+	private static final String URL_LIVE_TRAFFIC_FOND_IDF = "http://www.sytadin.fr/fonds/fond_IDF.jpg";
+	private static final String URL_LIVE_TRAFFIC = "http://www.sytadin.fr/sys/raster.jsp.html";
+	private static final String URL_QUICK_STATS = "http://www.sytadin.fr/opencms/sites/sytadin/sys/elements/iframe-direct.jsp.html";
+	private static final String URL_CLOSED_AT_NIGHT = "http://www.sytadin.fr/opencms/sites/sytadin/sys/fermetures.jsp.html";
+	private static final String URL_TRAFFIC_COLLISIONS_FR = "http://www.infotrafic.com/route.php?region=FRANC&link=accidents.php";
+	private static final String URL_TRAFFIC_COLLISIONS_IDF = "http://www.infotrafic.com/route.php?region=IDF&link=accidents.php";
+
+	private class SytadroidWebViewClient extends WebViewClient {
+		private int x;
+		private int y;
+
+		public SytadroidWebViewClient(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			resetTitle(view, url, "Loading...");
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			resetTitle(view, url, null);
+			webview.scrollTo(x, y);
+		}
+	}
+
+	WebView webview;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		webview = (WebView) findViewById(R.id.webview);
+		WebSettings settings = webview.getSettings();
+		settings.setJavaScriptEnabled(true);
+
+		// Cache resources
+		cacheResources();
+
+		// Default view
+		showLiveTrafficLite();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.liveTrafficLite:
+			showLiveTrafficLite();
+			return true;
+		case R.id.liveTraffic:
+			showLiveTraffic();
+			return true;
+		case R.id.quickStats:
+			showQuickStats();
+			return true;
+		case R.id.closedAtNight:
+			showClosedAtNight();
+			return true;
+		case R.id.trafficCollisions:
+			showTrafficCollisions();
+			return true;
+		}
+		return false;
+	}
+
+	private void loadUrlInWebview(String url, int scale, int x, int y) {
+		Log.i(SYTADROID, "Loading URL '" + url + "'");
+		webview.loadUrl(url);
+		webview.setInitialScale(scale);
+		webview.setWebViewClient(new SytadroidWebViewClient(x, y));
+	}
+
+	private void cacheResources() {
+		File file = getFileStreamPath(FILENAME_FOND_IDF);
+
+		if (file.exists()) {
+			Log.i(SYTADROID, "Resources already cached in '" + getFilesDir().getAbsolutePath() + "'");
+		} else {
+			try {
+				Log.i(SYTADROID, "Trying to download and save resources to '" + getFilesDir().getAbsolutePath() + "'");
+				HttpClient client = new DefaultHttpClient();
+				HttpGet get = new HttpGet(URL_LIVE_TRAFFIC_FOND_IDF);
+				HttpResponse response = client.execute(get);
+				HttpEntity responseEntity = response.getEntity();
+				byte[] bytes = new byte[(int) responseEntity.getContentLength()];
+				InputStream content = responseEntity.getContent();
+				int read = 0;
+				while (read < bytes.length) {
+					read = read + content.read(bytes, read, bytes.length);
+				}
+				content.close();
+				FileOutputStream fos = openFileOutput(FILENAME_FOND_IDF, Activity.MODE_WORLD_WRITEABLE);
+				fos.write(bytes);
+				fos.flush();
+				fos.close();
+				Log.i(SYTADROID, "Successfully downloaded resources");
+			} catch (Exception e) {
+				Log.e(SYTADROID, "Could not download and save resources", e);
+			}
+		}
+	}
+
+	private void showLiveTrafficLite() {
+		loadUrlInWebview("file:///android_asset/sytadroid.html", 200, 400, 100);
+	}
+
+	private void showLiveTraffic() {
+		loadUrlInWebview(URL_LIVE_TRAFFIC, 200, 100, 100);
+	}
+
+	private void showQuickStats() {
+		loadUrlInWebview(URL_QUICK_STATS, 150, 0, 0);
+	}
+
+	private void showClosedAtNight() {
+		loadUrlInWebview(URL_CLOSED_AT_NIGHT, 50, 100, 100);
+	}
+
+	private void showTrafficCollisions() {
+		loadUrlInWebview(URL_TRAFFIC_COLLISIONS_IDF, 100, 0, 0);
+	}
+
+	private void resetTitle(WebView view, String url, String title) {
+		if (url == null || title == null) {
+			WebHistoryItem item = view.copyBackForwardList().getCurrentItem();
+			if (item != null) {
+				if (url == null) {
+					url = item.getUrl();
+				}
+				if (title == null) {
+					title = item.getTitle();
+				}
+			}
+		}
+		setTitle(buildUrlTitle(url, title));
+	}
+
+	/**
+	 * Builds and returns the page title, which is some combination of the page
+	 * URL and title.
+	 * 
+	 * @param url
+	 *            The URL of the site being loaded.
+	 * @param title
+	 *            The title of the site being loaded.
+	 * @return The page title.
+	 */
+	private String buildUrlTitle(String url, String title) {
+		String urlTitle = "";
+
+		if (url != null) {
+			String titleUrl = buildTitleUrl(url);
+
+			if (title != null && 0 < title.length()) {
+				if (titleUrl != null && 0 < titleUrl.length()) {
+					urlTitle = titleUrl + ": " + title;
+				} else {
+					urlTitle = title;
+				}
+			} else {
+				if (titleUrl != null) {
+					urlTitle = titleUrl;
+				}
+			}
+		}
+
+		return urlTitle;
+	}
+
+	/**
+	 * @param url
+	 *            The URL to build a title version of the URL from.
+	 * @return The title version of the URL or null if fails. The title version
+	 *         of the URL can be either the URL hostname, or the hostname with
+	 *         an "https://" prefix (for secure URLs), or an empty string if,
+	 *         for example, the URL in question is a file:// URL with no
+	 *         hostname.
+	 */
+	private static String buildTitleUrl(String url) {
+		String titleUrl = null;
+
+		if (url != null) {
+			try {
+				// parse the url string
+				URL urlObj = new URL(url);
+				if (urlObj != null) {
+					titleUrl = "";
+
+					String protocol = urlObj.getProtocol();
+					String host = urlObj.getHost();
+
+					if (host != null && 0 < host.length()) {
+						titleUrl = host;
+						if (protocol != null) {
+							// if a secure site, add an "https://" prefix!
+							if (protocol.equalsIgnoreCase("https")) {
+								titleUrl = protocol + "://" + host;
+							}
+						}
+					}
+				}
+			} catch (MalformedURLException e) {
+			}
+		}
+
+		return titleUrl;
+	}
+
+}
