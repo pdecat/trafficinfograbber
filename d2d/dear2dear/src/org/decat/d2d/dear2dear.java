6cf91d5831235b98ba06876966e00eff11ad2262
pdecat <pdecat@48d36ac8-552b-11de-ac5b-d5db2185a7fc>
2010-03-25 10:58:35 +0000

    Création du projet dear2dear

---
+++ d2d/dear2dear/src/org/decat/d2d/dear2dear.java
@@ -0,0 +1,13 @@
+package org.decat.d2d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dear2dear extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
