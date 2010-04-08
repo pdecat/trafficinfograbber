6f6ab22db87f0f1298fb484711878c351d9f95c1
pdecat@gmail.com <pdecat@gmail.com@48d36ac8-552b-11de-ac5b-d5db2185a7fc>
2010-04-08 21:20:40 +0000

    Ménage

--- sandbox/Sandbox/src/org/decat/sandbox/ContactHelper.java
+++ sandbox/Sandbox/src/org/decat/sandbox/ContactHelper.java
@@ -3,7 +3,6 @@ package org.decat.sandbox;
 import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
-import android.provider.Contacts.People;
 import android.util.Log;
 
 public class ContactHelper {
@@ -24,29 +23,5 @@ public class ContactHelper {
 		Log.d(Sandbox.TAG, "Retrieved contact '" + contactUri + "' information " + column + "=" + result);
 
 		return result;
-	}
-
-	public static String getContactPhoneNumberFromUriString(Activity activity, String contactUri) {
-		String result = null;
-		String column = People.Phones.NUMBER;
-		Uri contactData = Uri.parse(contactUri);
-		Cursor c = null;
-		try {
-			c = activity.managedQuery(contactData, null, People.Phones.TYPE + "=" + People.Phones.TYPE_MOBILE, null, null);
-			if (c != null && c.moveToFirst()) {
-				result = c.getString(c.getColumnIndexOrThrow(column));
-			}
-		} finally {
-			if (c != null) {
-				c.close();
-			}
-		}
-		Log.d(Sandbox.TAG, "Retrieved contact '" + contactUri + "' information " + column + "=" + result);
-
-		return result;
-	}
-
-	public static String getContactNameFromUriString(Activity activity, String contactUri) {
-		return getContactInformation(activity, contactUri, People.NAME);
 	}
 }
