77a6dfb93c94c09bcfdb639b2bec3a3239cdbb1c
pdecat <pdecat@48d36ac8-552b-11de-ac5b-d5db2185a7fc>
2011-12-09 16:34:40 +0000

    Workaround the fact that onPageFinished is called only for main frame and that the picture rendering may not be done yet.

--- src/org/decat/tig/web/TIGWebViewClient.java
+++ src/org/decat/tig/web/TIGWebViewClient.java
@@ -5,6 +5,7 @@ import org.decat.tig.TIG;
 import org.decat.tig.preferences.PreferencesHelper;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
@@ -14,24 +15,31 @@ import com.google.ads.AdView;
 
 public class TIGWebViewClient extends WebViewClient {
 	private transient final Activity activity;
-	private transient int xScroll;
-	private transient int yScroll;
-	private transient String lastModified;
-	private transient String title;
+	private int initialScale;
+	private int xScroll;
+	private int yScroll;
+	private String lastModified;
+	private String title;
 
 	public TIGWebViewClient(Activity activity) {
 		this.activity = activity;
 	}
 
+	// @Override
+	// public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	// view.loadUrl(url);
+	// return true;
+	// }
+	//
 	@Override
-	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		view.loadUrl(url);
-		return true;
+	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+		Log.w(TIG.TAG, "Got error " + errorCode + " while loading URL " + failingUrl);
 	}
 
 	@Override
-	public void onLoadResource(WebView view, String url) {
+	public void onPageStarted(WebView view, String url, Bitmap favicon) {
 		setTitle(view, activity.getString(R.string.loading) + " " + title + "...");
+		setScaleAndScroll(view);
 
 		// Show the Ads banner if enabled
 		boolean showAds = TIG.getBooleanPreferenceValue(activity, PreferencesHelper.SHOW_ADS);
@@ -45,25 +53,42 @@ public class TIGWebViewClient extends WebViewClient {
 	}
 
 	@Override
-	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-		Log.w(TIG.TAG, "Got error " + errorCode + " while loading URL " + failingUrl);
-	}
-
-	@Override
 	public void onPageFinished(final WebView view, String url) {
 		String formattedTitle = title;
-		// if (lastModified != null) {
-		// try {
-		// SimpleDateFormat sdt = new SimpleDateFormat();
-		// lastModified = sdt.format(lastModified);
-		// } catch (Exception e) {
-		// Log.w(TIG.TAG, "Failed to parse last modified date '" + lastModified
-		// + "'", e);
-		// }
-		// formattedTitle += " - " + lastModified;
-		// }
+		if (lastModified != null) {
+			// try {
+			// SimpleDateFormat sdt = new SimpleDateFormat();
+			// lastModified = sdt.format(lastModified);
+			// } catch (Exception e) {
+			// Log.w(TIG.TAG, "Failed to parse last modified date '" +
+			// lastModified
+			// + "'", e);
+			// }
+			formattedTitle += " - " + lastModified;
+		}
 		setTitle(view, formattedTitle);
-		view.scrollTo(xScroll, yScroll);
+
+		// Set the scale and scroll once
+		setScaleAndScroll(view);
+
+		// Set the scale and scroll a second after some time because
+		// onPageFinished is called only for main frame.
+		// When onPageFinished() is called, the picture rendering may not be
+		// done yet.
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				view.post(new Runnable() {
+					public void run() {
+						setScaleAndScroll(view);
+					}
+				});
+			}
+		}).start();
 
 		// Hide the Ads banner after some time
 		new Thread(new Runnable() {
@@ -80,6 +105,13 @@ public class TIGWebViewClient extends WebViewClient {
 				});
 			}
 		}).start();
+
+		super.onPageFinished(view, url);
+	}
+
+	private void setScaleAndScroll(WebView view) {
+		view.setInitialScale(initialScale);
+		view.scrollTo(xScroll, yScroll);
 	}
 
 	private void setAdsVisibility(boolean visibility) {
@@ -89,6 +121,10 @@ public class TIGWebViewClient extends WebViewClient {
 
 	private void setTitle(WebView view, String title) {
 		activity.setTitle(title);
+	}
+
+	public void setInitialScale(int i) {
+		this.initialScale = i;
 	}
 
 	public void setOffset(int x, int y) {
