b76f5e753ce05c67c26a46d8f0025ad5aff52931
pdecat <pdecat@48d36ac8-552b-11de-ac5b-d5db2185a7fc>
2012-09-07 23:26:15 +0000

    Refactor WebViewClient thread handling using AndroidAnnotations. Introduce Retry Count Down on Page Loading Error

--- src/org/decat/tig/web/TIGWebViewClient.java
+++ src/org/decat/tig/web/TIGWebViewClient.java
@@ -27,45 +27,19 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.google.ads.AdView;
 import com.googlecode.androidannotations.annotations.AfterInject;
+import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.EBean;
 import com.googlecode.androidannotations.annotations.RootContext;
+import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 @EBean
 public class TIGWebViewClient extends WebViewClient {
 	private static final int ADS_DISPLAY_DURATION = 5000;
-
-	private final class HideAdsJob implements Runnable {
-		private final WebView view;
-		private long loadCount;
-
-		public HideAdsJob(WebView view, long loadCount) {
-			this.view = view;
-			this.loadCount = loadCount;
-		}
-
-		public void run() {
-			try {
-				Thread.sleep(ADS_DISPLAY_DURATION);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			// Hide ads only if no other loading has been triggered since this job was instantiated
-			if (loadCount == TIGWebViewClient.this.loadCount) {
-				view.post(new Runnable() {
-					public void run() {
-						Log.d(TIG.TAG, "TIGWebViewClient.HideAdsJob.run: loadCount=" + loadCount);
-
-						setAdsVisibility(false);
-					}
-				});
-			}
-		}
-	}
 
 	@RootContext
 	protected Activity activity;
@@ -84,7 +58,6 @@ public class TIGWebViewClient extends WebViewClient {
 
 	// Fields to manage ads display
 	private long loadCount = 0;
-	private Runnable hideAdsJob;
 
 	private int topPaddingPx;
 
@@ -102,14 +75,60 @@ public class TIGWebViewClient extends WebViewClient {
 	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
 		super.onReceivedError(view, errorCode, description, failingUrl);
 
+		startRetryCountDown();
+
 		Log.w(TIG.TAG, "TIGWebViewClient.onReceivedError: Got error " + description + " (" + errorCode + ") while loading URL " + failingUrl);
+	}
+
+	@Background
+	protected void startRetryCountDown() {
+		Log.d(TIG.TAG, "TIGWebViewClient.startRetryCountDown");
+
+		TextView tv = (TextView) activity.findViewById(R.id.retryCountDownText);
+
+		// Check if another count down is already in progress
+		String currentCountDown = tv.getText().toString();
+		if (Integer.parseInt(currentCountDown) > 0) {
+			// Abort
+			Log.d(TIG.TAG, "TIGWebViewClient.startRetryCountDown: another count down is already in progress, aborting (" + currentCountDown + "s left).");
+			return;
+		}
+
+		// Display retry count down
+		View rcd = activity.findViewById(R.id.retryCountDown);
+
+		setRetryCountDownVisibility(rcd, View.VISIBLE);
+
+		for (int c = 30; c >= 0; c--) {
+			updateRetryCountDown(tv, Integer.toString(c));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		setRetryCountDownVisibility(rcd, View.INVISIBLE);
+
+		// Trigger refresh
+		Log.d(TIG.TAG, "TIGWebViewClient.startRetryCountDown: trigger refresh (TODO)");
+	}
+
+	@UiThread
+	protected void setRetryCountDownVisibility(View v, int visibility) {
+		v.setVisibility(visibility);
+	}
+
+	@UiThread
+	protected void updateRetryCountDown(TextView tv, String text) {
+		tv.setText(text);
 	}
 
 	@Override
 	public void onPageStarted(WebView view, String url, Bitmap favicon) {
 		Log.d(TIG.TAG, "TIGWebViewClient.onPageStarted: url=" + url);
 		super.onPageStarted(view, url, favicon);
-		
+
 		// Store main URL
 		mainURL = url;
 	}
@@ -123,15 +142,10 @@ public class TIGWebViewClient extends WebViewClient {
 		// Show ads if checked in preferences
 		showAds = TIG.getBooleanPreferenceValue(activity, PreferencesHelper.SHOW_ADS);
 		if (showAds) {
-			view.post(new Runnable() {
-				public void run() {
-					setAdsVisibility(true);
-				}
-			});
-			
-			// Increment loadCount and instantiate future job to hide ads
+			// Increment loadCount
 			loadCount++;
-			hideAdsJob = new HideAdsJob(view, loadCount);
+
+			showAds();
 		}
 	}
 
@@ -152,7 +166,7 @@ public class TIGWebViewClient extends WebViewClient {
 		// Set the scale and scroll
 		setScaleAndScroll(view);
 	}
-	
+
 	@Override
 	public void onPageFinished(final WebView view, String url) {
 		super.onPageFinished(view, url);
@@ -166,71 +180,60 @@ public class TIGWebViewClient extends WebViewClient {
 		}
 		setTitle(view, formattedTitle);
 
-		// Set the scale and scroll once
+		// Set the scale and scroll
 		setScaleAndScroll(view);
 
-		// Schedule another zooming again after some time because
-		// onPageFinished is called only for main frame.
-		// When onPageFinished() is called, the picture rendering may not be
-		// done yet.
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					Thread.sleep(300);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				view.post(new Runnable() {
-					public void run() {
-						setScaleAndScroll(view);
-					}
-				});
-			}
-		}).start();
-
 		// Add padding to the top of the HTML view to compensate for the overlaid action bar on Android 3.0+
-		if (Integer.parseInt(Build.VERSION.SDK) >= 11) {
+		if (Build.VERSION.SDK_INT >= 11) {
 			view.loadUrl("javascript:document.body.style.paddingTop='" + topPaddingPx + "px'");
 		}
 
-		// Schedule ads hiding
+		// Hide ads after a short delay
 		if (showAds) {
-			new Thread(hideAdsJob).start();
+			hideAds(loadCount);
 		}
 	}
 
-	private void setScaleAndScroll(WebView view) {
-		view.setInitialScale(initialScale);
-		view.scrollTo(xScroll, yScroll);
+	@UiThread
+	protected void showAds() {
+		setAdsVisibility(true);
+	}
+
+	@UiThread(delay = ADS_DISPLAY_DURATION)
+	protected void hideAds(long loadCountBeforeDelay) {
+		Log.d(TIG.TAG, "TIGWebViewClient.hideAds: loadCountBeforeDelay=" + loadCountBeforeDelay + ", loadCount=" + loadCount);
+
+		// Hide ads only if no other loading has been triggered since this job was instantiated
+		if (loadCountBeforeDelay == TIGWebViewClient.this.loadCount) {
+			setAdsVisibility(false);
+		}
 	}
 
 	private void setAdsVisibility(boolean visibility) {
 		adview.setVisibility(visibility ? View.VISIBLE : View.GONE);
 	}
 
-	private void setTitle(WebView view, String title) {
+	@UiThread
+	protected void setScaleAndScroll(WebView view) {
+		view.setInitialScale(initialScale);
+		view.scrollTo(xScroll, yScroll);
+	}
+
+	@UiThread
+	protected void setTitle(WebView view, String title) {
 		activity.setTitle(title);
 	}
 
-	public void setInitialScale(int i) {
-		this.initialScale = i;
-	}
-
-	public void setOffset(int x, int y) {
-		this.xScroll = x;
-		this.yScroll = y;
+	public void setParameters(String title, String lastModified, int initialScale, int xoffset, int yoffset) {
+		this.title = title;
+		this.lastModified = lastModified;
+		this.initialScale = initialScale;
+		this.xScroll = xoffset;
+		this.yScroll = yoffset;
 
 		// Compensate the padding at the top of the HTML view that compensates for the overlaid action bar on Android 3.0+
-		if (Integer.parseInt(Build.VERSION.SDK) >= 11) {
+		if (Build.VERSION.SDK_INT >= 11) {
 			this.yScroll += topPaddingPx;
 		}
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setLastModified(String lastModified) {
-		this.lastModified = lastModified;
 	}
 }
