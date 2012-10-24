f8fda3bebb741929ff64824bad49bf7ad43f9f81
pdecat <pdecat@48d36ac8-552b-11de-ac5b-d5db2185a7fc>
2012-10-24 20:19:33 +0000

    Try to fix loading errors detection

--- tig/TrafficInfoGrabber/src/org/decat/tig/web/TIGWebViewClient.java
+++ tig/TrafficInfoGrabber/src/org/decat/tig/web/TIGWebViewClient.java
@@ -129,6 +129,7 @@ public class TIGWebViewClient extends WebViewClient {
 	@Override
 	public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
 		Log.w(TIG.TAG, "TIGWebViewClient.onReceivedError: Got error " + description + " (" + errorCode + ") while loading URL " + failingUrl);
+		view.stopLoading();
 		startRetryCountDown();
 	}
 
