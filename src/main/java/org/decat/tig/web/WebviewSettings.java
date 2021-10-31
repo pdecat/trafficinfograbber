3e599132818a33fda2741ca35b9ace2a499b6798
Patrick Decat <pdecat@gmail.com>
2021-10-31 11:17:52 +0100

    Update licence headers and use HTTPS links

--- src/main/java/org/decat/tig/web/WebviewSettings.java
+++ src/main/java/org/decat/tig/web/WebviewSettings.java
@@ -1,7 +1,7 @@
-/**
+/*
  * TrafficInfoGrabber
  *
- * Copyright (C) 2010 - 2018 Patrick Decat
+ * Copyright (C) 2010 - 2021 Patrick Decat
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
@@ -9,36 +9,11 @@
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
  */
 package org.decat.tig.web;
 
-/*
- * #%L
- * TrafficInfoGrabber
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2010 - 2014 Patrick Decat
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
 public class WebviewSettings {
-
 	public final String title;
 	public final String url;
 	public final int xmin;
